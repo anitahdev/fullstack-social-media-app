8e0cd0714b434565844d1bc0174cb6d81496d0da
Anita Horoszko <95635795+anitahdev@users.noreply.github.com>
2022-12-13 15:13:34 +0100

    wip

---
+++ api/controllers/comment.js
@@ -0,0 +1,12 @@
+import { db } from "../connect.js";
+
+export const getComments = (req, res) => {
+  const q = `SELECT c.*, u.id AS userId, name, profilePic FROM comments AS c JOIN users AS u ON (u.id = c.userId)
+    WHERE c.postId = ? ORDER BY c.createdAt DESC
+    `;
+
+  db.query(q, [req.query.postId], (err, data) => {
+    if (err) return res.status(500).json(err);
+    return res.status(200).json(data);
+  });
+};

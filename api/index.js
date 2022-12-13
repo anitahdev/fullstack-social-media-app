20b02b18b65ff4f288e49e2a6c31ce042b3f6321
Anita Horoszko <95635795+anitahdev@users.noreply.github.com>
2022-12-13 15:36:38 +0100

    finished project

--- api/index.js
+++ api/index.js
@@ -1,5 +1,4 @@
 import express from "express";
-const app = express();
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
@@ -9,8 +8,7 @@ import relationshipRoutes from "./routes/relationships.js";
 import cors from "cors";
 import multer from "multer";
 import cookieParser from "cookie-parser";
-
-
+const app = express();
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", true);
   next();
